50000fa407019b866aff87995417c106c93ea2ba
Mike Murphy <mmurphy2@coastal.edu>
2022-12-03 21:04:24 -0500

    Minor tweaks to PeekReader; added docstrings.

--- libteal/peek_reader.py
+++ libteal/peek_reader.py
@@ -25,19 +25,43 @@
 #
 
 
-# TODO: docstrings
-
-
 from io import IOBase, SEEK_SET, SEEK_CUR, SEEK_END
 from os import pread
 
 
 class PeekReader(IOBase):
-    def __init__(self, base_fh_or_fd):
+    '''
+    Implements a file-like object for reading the contents of files that are
+    already opened in read-write mode (i.e. opened with the w+b flag). Such
+    files include those produced by the tempfile module when using default
+    settings. Unlike simply opening and reading the contents of the file
+    from disk, the PeekReader reuses the file descriptor with the os.pread()
+    method. Content that has been written to the file by another process
+    or thread can be read without changing the write offset.
+    '''
+    def __init__(self, base_fh_or_fd, text=True):
+        '''
+        Constructor.
+
+        base_fh_or_fd   --  File object or file descriptor
+        text            --  Convert bytestrings to Unicode
+
+        Note that, if a file-like object is passed for the base_fh_or_fd,
+        this object MUST support the fileno() method and have a valid file
+        descriptor. The "peek reading" is done using os.pread, which is a
+        wrapper on pread(2) declared in the C unistd.h header.
+
+        Optionally, the read(), readline(), and readlines() methods can
+        return Unicode strings instead of bytestrings, if the text
+        argument is True (the default).
+        '''
         IOBase.__init__(self)
+
         self.fh = None
         self.fd = None
         self.is_closed = True
+        self.text = text
+
         if type(base_fh_or_fd) is int:
             self.fd = base_fh_or_fd
             self.is_closed = False
@@ -46,24 +70,57 @@ class PeekReader(IOBase):
             self.fd = self.fh.fileno()
             self.is_closed = False
         #
+
+        # We have to maintain our own offset, since the underlying file is
+        # potentially in use already for writing
         self.position = 0
     #
     def close(self):
+        '''
+        Closes the PeekReader, which prevents further reading of the file.
+        This operation does NOT close the underlying file descriptor, since
+        that may still be in use by the writer thread or process. The
+        internal references to the file handle and/or descriptor are cleared,
+        so as not to interfere with regular garbage collection of the
+        actual file object used by the writer.
+        '''
         self.is_closed = True
+        self.fh = None
+        self.fd = None
     #
     def fileno(self):
+        '''
+        Returns the file descriptor number to which this instance is bound,
+        if any. If no instance is bound, returns None.
+        '''
         return self.fd
     #
     def flush(self):
+        '''
+        Does nothing, since writing is not supported.
+        '''
         pass
     #
     def isatty(self):
+        '''
+        Returns False, since the PeekReader does not implement a tty.
+        '''
         return False
     #
     def readable(self):
+        '''
+        Returns True iff the PeekReader is open with a file descriptor set.
+        Note that corner cases may arise (e.g. when attached to a file
+        belonging to another process) in which reading is not actually
+        possible.
+        '''
         return not self.is_closed
     #
-    def read(self, size=65536):
+    def _read(self, size=65536):
+        '''
+        Reads at most size bytes from the file descriptor. This is the
+        private implementation that returns a bytestring.
+        '''
         if not self.is_closed:
             raw = pread(self.fd, size, self.position)
             self.position += len(raw)
@@ -72,13 +129,47 @@ class PeekReader(IOBase):
         #
         return raw
     #
+    def read(self, size=65536):
+        '''
+        Reads at most size (default 64K) bytes from the file descriptor.
+        Returns a string if the text property of this object is True,
+        otherwise returns a bytestring. Note that the length of the
+        returned string is limited to the read size. For multibyte Unicode
+        characters, the length of a translated string will be even shorter.
+        '''
+        raw = self._read(size)
+        return str(raw, 'utf-8') if self.text else raw
+    #
     def read_to(self, size=-1, sentinel=None):
+        '''
+        Reads data from the file until the specified number of bytes have
+        been read, the first occurrence is encountered of a sentinel
+        value, or the end of the file is reached.
+
+        size      --  Maximum number of bytes to read (negative for unlimited)
+        sentinel  --  Sentinel value at which to stop reading if encountered
+        text      --  Override Unicode conversion if not None
+
+        A negative value for size will read to the end of the file if no
+        sentinel has been set. Note that the end of the file in this case
+        means the end of the data that has been written by the writer
+        process or thread.
+
+        If a sentinel value is used as the stopping condition, the sentinel
+        must be passed as a bytestring. Reading will continue until either
+        the sentinel value is first seen or the size limit is reached. If
+        encountered, the sentinel will be appended to the end of the returned
+        string.
+
+        This method returns a bytestring or a Unicode string depending on
+        the text property of the PeekReader object.
+        '''
         happy = False
         full = b''
 
         while not happy:
             rsize = 65536 if size < 0 else size
-            chunk = self.read(rsize)
+            chunk = self._read(rsize)
 
             if len(chunk) == 0:
                 # At EOF: nothing to do
@@ -96,15 +187,50 @@ class PeekReader(IOBase):
             #
         #
 
-        return full
+        return str(full, 'utf-8') if self.text else full
+    #
+    def readall(self):
+        '''
+        Reads and returns all the content of the underlying file. The
+        return type is a Unicode or byte string, depending on the text
+        property of the PeekReader object.
+        '''
+        self.seek(0)
+        return self.read_to()
     #
     def readline(self, size=-1):
-        return str(self.read_to(size=size, sentinel=b'\n'), 'utf-8')
+        '''
+        Reads and returns one line from the underlying file. If size is
+        nonnegative, at most size bytes will be read, even if a partial
+        line results. The line separator is b'\\n'.
+        '''
+        return self.read_to(size=size, sentinel=b'\n')
     #
     def readlines(self, hint=-1):
-        return str(self.read_to(size=hint), 'utf-8').splitlines()
+        '''
+        Reads and returns all lines in the underlying file, starting from
+        the current offset. If hint is nonnegative, reads at most hint
+        bytes, even if a partial last line results. The line separator
+        depends upon the text processing mode of the PeekReader, since
+        line separation occurs only after all the text has been read and
+        optionally converted to Unicode.
+        '''
+        return self.read_to(size=hint).splitlines()
     #
     def seek(self, offset, whence=SEEK_SET):
+        '''
+        Changes the stream position to the given byte offset.
+
+        offset   --  Byte offset to which to seek
+        whence   --  Offset mode (SEEK_SET, SEEK_CUR, or SEEK_END)
+
+        Note that the "end" of the file is the current writer position.
+        This position can only be determined if the PeekReader was
+        instantiated from a file object, so that the tell() method of that
+        file object can be called. If the PeekReader was instantiated
+        from a file descriptor, then SEEK_END behaves the same as SEEK_CUR,
+        since the end location is unknown.
+        '''
         if whence == SEEK_CUR:
             self.position = self.position + offset
         elif whence == SEEK_END:
@@ -119,12 +245,23 @@ class PeekReader(IOBase):
         #
     #
     def seekable(self):
+        '''
+        Returns True if the file is not closed.
+        '''
         return not self.is_closed
     #
     def tell(self):
+        '''
+        Returns the current position (byte offset) of the PeekReader. This
+        position is independent of the offset of the writer thread or
+        process.
+        '''
         return self.position
     #
     def writable(self):
+        '''
+        Returns False. The PeekReader is for reading only, hence the name.
+        '''
         return False
     #
 #
